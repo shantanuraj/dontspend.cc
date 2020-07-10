ec33c984a33beed000e682fab0bb5d4bf48c5c31
Shantanu Raj <s@sraj.me>
2020-07-10 23:27:54 +0530

    feat(extension): Add base extension

---
+++ extension/src/options/options.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'normalize.css'
+import './options.css'
+
+function App () {
+  return (
+    <div>
+      Hello, Options!
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,  document.getElementById('app'))
---
+++ extension/src/popup/popup.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'normalize.css'
+import './popup.css'
+
+function App () {
+  return (
+    <div>
+      Hello, Popup!
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,  document.getElementById('app'))
